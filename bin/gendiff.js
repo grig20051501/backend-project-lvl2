7f3f231ec1fe75df19559262418bc86608e3872b
Grisha <grisha_pitsuha@mail.ru>
2022-07-19 23:30:03 +0300

    feat: add getData function and test files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+
+const getData = (filepath) => path.resolve(readFileSync(filepath));
+
+const getPath = (program, numOfPath) => program.args[numOfPath - 1];
 
 const program = new Command();
 program.description('Compares two configuration files and showes a difference')
@@ -7,3 +13,11 @@ program.description('Compares two configuration files and showes a difference')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format', 'output format');
 program.parse();
+
+const path1 = getPath(program, 1);
+const path2 = getPath(program, 2);
+const data1 = JSON.parse(getData(path1));
+const data2 = JSON.parse(getData(path2));
+/* const compareData = (dataFirst, dataSecond) => {
+
+} */
